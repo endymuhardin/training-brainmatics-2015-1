0b5b0f84847850a1f0526e149710df27fef2ec7d
Endy Muhardin <endy.muhardin@gmail.com>
2015-09-02 14:26:56 +0700

    implement REST API untuk data peserta

---
+++ aplikasi-pelatihan/src/main/java/com/muhardin/endy/pelatihan/controller/PesertaController.java
@@ -0,0 +1,56 @@
+package com.muhardin.endy.pelatihan.controller;
+
+import com.muhardin.endy.pelatihan.dao.PesertaDao;
+import com.muhardin.endy.pelatihan.entity.Peserta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PesertaController {
+    
+    @Autowired
+    private PesertaDao pd;
+    
+    @RequestMapping(value="/peserta", method = RequestMethod.GET)
+    public Page<Peserta> cariPeserta(Pageable page){
+        return pd.findAll(page);
+    }
+    
+    @RequestMapping(value="/peserta", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void insertPesertaBaru(@RequestBody Peserta p){
+        pd.save(p);
+    }
+    
+    @RequestMapping(value="/peserta/{id}", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.OK)
+    public void updatePeserta(@PathVariable("id") String id, @RequestBody Peserta p){
+        p.setId(id);
+        pd.save(p);
+    }
+    
+    @RequestMapping(value="/peserta/{id}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Peserta> cariPesertaById(@PathVariable("id") String id){
+        Peserta hasil = pd.findOne(id);
+        if(hasil == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(hasil, HttpStatus.OK);
+    }
+    
+    @RequestMapping(value="/peserta/{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.OK)
+    public void hapusPeserta(@PathVariable("id") String id){
+        pd.delete(id);
+    }
+}
