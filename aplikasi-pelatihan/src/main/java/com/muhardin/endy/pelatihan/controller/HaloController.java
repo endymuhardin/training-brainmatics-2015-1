7823ffd30d857ec14d8850f42ae026830f6a6058
Endy Muhardin <endy.muhardin@gmail.com>
2015-09-03 14:37:36 +0700

    templating dengan thymeleaf

--- aplikasi-pelatihan/src/main/java/com/muhardin/endy/pelatihan/controller/HaloController.java
+++ aplikasi-pelatihan/src/main/java/com/muhardin/endy/pelatihan/controller/HaloController.java
@@ -3,19 +3,30 @@ package com.muhardin.endy.pelatihan.controller;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HaloController {
     
-    @RequestMapping("/halo")
-    public Map<String, Object> halo(
+    @RequestMapping("/halorest")
+    @ResponseBody
+    public Map<String, Object> haloRest(
             @RequestParam(value="nama", required = false) String nama){
         Map<String, Object> hasil = new HashMap<>();
         hasil.put("nama", nama);
         hasil.put("waktu", new Date());
         return hasil;
+    }
+    
+    @RequestMapping("/halo")
+    public void haloHtml(@RequestParam(value="nama", required = false) String nama, 
+            Model hasil){
+        hasil.addAttribute("nama", nama);
+        hasil.addAttribute("waktu", new Date());
     }
 }
