a8fcfd639c7cbb3f1cd261c9f0485d3f0df1f215
Endy Muhardin <endy.muhardin@gmail.com>
2015-09-04 16:43:42 +0700

    upload file

--- aplikasi-pelatihan/src/main/java/com/muhardin/endy/pelatihan/controller/PesertaHtmlController.java
+++ aplikasi-pelatihan/src/main/java/com/muhardin/endy/pelatihan/controller/PesertaHtmlController.java
@@ -2,6 +2,7 @@ package com.muhardin.endy.pelatihan.controller;
 
 import com.muhardin.endy.pelatihan.dao.PesertaDao;
 import com.muhardin.endy.pelatihan.entity.Peserta;
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.validation.Valid;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/peserta")
@@ -58,11 +60,28 @@ public class PesertaHtmlController {
     }
 
     @RequestMapping(value = "/form", method = RequestMethod.POST)
-    public String prosesForm(@Valid Peserta p, BindingResult errors) {
+    public String prosesForm(@Valid Peserta p, BindingResult errors, 
+            MultipartFile foto) throws Exception {
         if (errors.hasErrors()) {
             return "/peserta/form";
         }
         pd.save(p);
+        
+        String namaFile = foto.getName();
+        String jenisFile = foto.getContentType();
+        String namaAsli = foto.getOriginalFilename();
+        Long ukuran = foto.getSize();
+        
+        System.out.println("Nama file : "+namaFile);
+        System.out.println("Jenis file : "+jenisFile);
+        System.out.println("Nama asli : "+namaAsli);
+        System.out.println("Ukuran : "+ukuran);
+        
+        
+        File tujuan = new File("foto.png");
+        foto.transferTo(tujuan);
+        System.out.println("File sudah dicopy ke :"+tujuan.getAbsolutePath());
+        
         return "redirect:list";
     }
 }
