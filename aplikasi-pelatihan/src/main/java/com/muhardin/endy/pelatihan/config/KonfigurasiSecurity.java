168365a226b517f48b13c6c2af9a7a65ed38cb66
Endy Muhardin <endy.muhardin@gmail.com>
2015-09-04 09:40:33 +0700

    enable login dengan Spring Security

---
+++ aplikasi-pelatihan/src/main/java/com/muhardin/endy/pelatihan/config/KonfigurasiSecurity.java
@@ -0,0 +1,35 @@
+package com.muhardin.endy.pelatihan.config;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class KonfigurasiSecurity extends WebSecurityConfigurerAdapter {
+
+    private static final String SQL_LOGIN
+            = "select username, password, active as enabled "
+            + "from s_users where username = ?";
+    
+    private static final String SQL_PERMISSION
+            = "select u.username, r.nama as authority "
+            + "from s_users u join s_user_role ur on u.id = ur.id_user "
+            + "join s_roles r on ur.id_role = r.id "
+            + "where u.username = ?";
+    
+    @Autowired private DataSource ds;
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()
+                .dataSource(ds)
+                .usersByUsernameQuery(SQL_LOGIN)
+                .authoritiesByUsernameQuery(SQL_PERMISSION);
+    }
+
+}
---
+++ aplikasi-pelatihan/src/test/resources/data/security.sql
@@ -0,0 +1,49 @@
+drop table s_user_rold;
+drop table role;
+drop table s_users;
+
+create table s_users (
+    id VARCHAR(36) PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    active BOOLEAN
+);
+
+insert into s_users (id, username, password, active)
+values('u001', 'endy', '123', true);
+
+insert into s_users (id, username, password, active)
+values('u002', 'adi', '123', true);
+
+create table s_roles (
+    id VARCHAR(36) PRIMARY KEY,
+    nama VARCHAR(255) NOT NULL
+);
+
+insert into s_roles (id, nama)
+values ('adm', 'ROLE_ADMIN');
+
+insert into s_roles (id, nama)
+values ('stf', 'ROLE_STAFF');
+
+
+create table s_user_role (
+    id_user VARCHAR(36) NOT NULL,
+    id_role VARCHAR(36) NOT NULL
+);
+
+insert into s_user_role (id_user, id_role)
+values ('u001', 'adm');
+
+insert into s_user_role (id_user, id_role)
+values ('u001', 'stf');
+
+insert into s_user_role (id_user, id_role)
+values ('u002', 'stf');
+
+select username, password, active as enabled from s_users where username = 'endy';
+
+select r.nama as authority 
+from s_users u join s_user_role ur on u.id = ur.id_user
+join s_roles r on ur.id_role = r.id
+where u.username = 'endy';
