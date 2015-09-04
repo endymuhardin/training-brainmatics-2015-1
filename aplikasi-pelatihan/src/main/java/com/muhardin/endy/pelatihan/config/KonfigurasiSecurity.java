00e44b89063a4c11aecf69b0e8c182e861e03c2d
Endy Muhardin <endy.muhardin@gmail.com>
2015-09-04 10:24:23 +0700

    custom login/logout

--- aplikasi-pelatihan/src/main/java/com/muhardin/endy/pelatihan/config/KonfigurasiSecurity.java
+++ aplikasi-pelatihan/src/main/java/com/muhardin/endy/pelatihan/config/KonfigurasiSecurity.java
@@ -4,25 +4,27 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebMvcSecurity
 public class KonfigurasiSecurity extends WebSecurityConfigurerAdapter {
 
     private static final String SQL_LOGIN
             = "select username, password, active as enabled "
             + "from s_users where username = ?";
-    
+
     private static final String SQL_PERMISSION
             = "select u.username, r.nama as authority "
             + "from s_users u join s_user_role ur on u.id = ur.id_user "
             + "join s_roles r on ur.id_role = r.id "
             + "where u.username = ?";
-    
-    @Autowired private DataSource ds;
-    
+
+    @Autowired
+    private DataSource ds;
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
@@ -30,6 +32,19 @@ public class KonfigurasiSecurity extends WebSecurityConfigurerAdapter {
                 .dataSource(ds)
                 .usersByUsernameQuery(SQL_LOGIN)
                 .authoritiesByUsernameQuery(SQL_PERMISSION);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/halo")
+                .and()
+                .logout();
     }
 
 }
